591762f9e234228127a3556a14ba93327dec74dc
jensroes <jens.roes@mail.com>
2023-01-13 11:35:31 +0000

    Change typing to digital

--- scripts/extract posterior/get_posterior_for_plotting.R
+++ scripts/extract posterior/get_posterior_for_plotting.R
@@ -8,15 +8,16 @@ theme_set(theme_bw() +
 
 # Load posterior
 m <- readRDS("stanout/mv_time_x_modality_poly2.rda")
-#m <- readRDS("stanout/mv_time_x_modality_x_covars_poly2.rda")
 
 data <- m$data %>%
   mutate(across(c(advanced_structures, events:terminators_total), as.numeric),
-         time = time + 1) %>%
-  pivot_longer(c(advanced_structures, events:terminators_total)) %>%
+         time = time + 1,
+         across(correct_spaces, ~./ spacing_total),
+         across(correct_terminators, ~./ terminators_total),
+         across(spelling_correct, ~./number_words)) %>%
+  pivot_longer(c(advanced_structures, events:correct_spaces, correct_terminators)) %>%
   select(time, modality, dv = name, value) %>%
-  mutate(dv = str_remove_all(dv, "_")) %>%
-  filter(!grepl(x = dv, pattern = "total"))
+  mutate(dv = str_remove_all(dv, "_")) 
 
 # Extract conditional effects for plotting
 ces <- conditional_effects(m, re_formula = NA) 
@@ -33,12 +34,17 @@ ps <- map(idx, ~ces[.x] %>%
             as_tibble() %>%
             mutate(dv = .x)) %>%
   bind_rows() %>%
-  select(dv, time, modality, est = estimate__, lo = lower__, up = upper__) %>%
+  select(dv, time, modality, 
+         est = estimate__, 
+         lo = lower__, 
+         up = upper__,
+         spacing_total, number_words, terminators_total) %>%
   separate(dv, into = c("dv", "remove"), sep = "\\.") %>%
   select(-remove) %>%
   mutate(time = time + 1)
 
 write_csv(ps, "stanout/posterior_for_plotting.csv")
+
 
 data_means <- data %>% group_by(dv, time, modality) %>%
   summarise(mean = mean(value),
@@ -46,14 +52,18 @@ data_means <- data %>% group_by(dv, time, modality) %>%
             lo = mean - 1.96 * se,
             up = mean + 1.96 * se)
 
-ggplot(ps, aes(x = time, y = est, colour = modality, fill = modality)) +
+ps %>% 
+  mutate(across(c(est, lo, up), ~ifelse(dv == "spellingcorrect", . / number_words, 
+                                               ifelse(dv == "correctspaces", . / spacing_total, 
+                                                      ifelse(dv == "correctterminators", . / terminators_total, .))))) %>% 
+  ggplot(aes(x = time, y = est, colour = modality, fill = modality)) +
   geom_ribbon(aes(ymin = lo, ymax = up), colour = NA, alpha =  .1) +
   geom_line(size = 1) +
-#  geom_point(data = data_means, aes(y = mean), size = 2, position = position_dodge(.5)) +
- # geom_errorbar(data = data_means, aes(y = mean, ymin = lo, ymax = up), 
-  #             width = .1, position = position_dodge(.5)) +
-#  geom_line(data = data_means, aes(y = mean), size = .5, alpha = .5,
-#            linetype = "dashed", position = position_dodge(.5)) +
+  geom_point(data = data_means, aes(y = mean), size = 2, position = position_dodge(.5)) +
+  geom_errorbar(data = data_means, aes(y = mean, ymin = lo, ymax = up), 
+               width = .1, position = position_dodge(.5)) +
+  geom_line(data = data_means, aes(y = mean), size = .5, alpha = .5,
+            linetype = "dashed", position = position_dodge(.5)) +
   facet_wrap(~dv, scales = "free") +
   scale_color_brewer(palette = "Dark2") +
   scale_fill_brewer(palette = "Dark2") +
